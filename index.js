d47c2cb0b985e4c10c3df50b64a7666556700ba3
Vladislav Smirnov <vdmsmirnov@gmail.com>
2017-01-20 17:10:11 +0300

    Style fixes, clean, increase page load time

--- index.js
+++ index.js
@@ -12,38 +12,42 @@ window.onload = () => {
 
     document.getElementById('technology').addEventListener('click', () => {
         technologyPage.style.visibility = 'visible';
-        document.body.style.overflow = 'hidden';
+        document.body.style.overflowY = 'hidden';
     });
 
     technologyPage.getElementsByClassName('close')[0].addEventListener('click', () => {
         technologyPage.style.visibility = 'hidden';
+        document.body.style.overflowY = 'scroll';
     });
 
     document.getElementById('calendar').addEventListener('click', () => {
         calendarPage.style.visibility = 'visible';
-        document.body.style.overflow = 'hidden';
+        document.body.style.overflowY = 'hidden';
     });
 
     calendarPage.getElementsByClassName('close')[0].addEventListener('click', () => {
         calendarPage.style.visibility = 'hidden';
+        document.body.style.overflowY = 'scroll';
     });
 
     document.getElementById('training').addEventListener('click', () => {
         trainingPage.style.visibility = 'visible';
-        document.body.style.overflow = 'hidden';
+        document.body.style.overflowY = 'hidden';
     });
 
     trainingPage.getElementsByClassName('close')[0].addEventListener('click', () => {
         trainingPage.style.visibility = 'hidden';
+        document.body.style.overflowY = 'scroll';
     });
 
     document.getElementById('solution').addEventListener('click', () => {
         solutionPage.style.visibility = 'visible';
-        document.body.style.overflow = 'hidden';
+        document.body.style.overflowY = 'hidden';
     });
 
     solutionPage.getElementsByClassName('close')[0].addEventListener('click', () => {
         solutionPage.style.visibility = 'hidden';
+        document.body.style.overflowY = 'scroll';
     });
 
     new Siema({

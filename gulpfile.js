898865f14b6a9b9fcdfe47dc719c35406b56f7ba
Vladislav Smirnov <vdmsmirnov@gmail.com>
2017-01-25 16:11:25 +0300

    Implement gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,8 @@ const gulp = require('gulp'),
     gulpSequence = require('gulp-sequence'),
     rename = require("gulp-rename"),
     concatCss = require('gulp-concat-css'),
-    del = require('del');
+    del = require('del'),
+    livereload = require('gulp-livereload');
 
 gulp.task('imagemin', () => {
     return gulp.src('photos/*.png')
@@ -27,7 +28,8 @@ gulp.task('cleanJs', () => {
     return gulp.src('./src/home.js')
         .pipe(cleanJs())
         .pipe(rename('index.js'))
-        .pipe(gulp.dest(''));
+        .pipe(gulp.dest(''))
+        .pipe(livereload());
 });
 
 gulp.task('concatHtml', () => {
@@ -42,7 +44,8 @@ gulp.task('clearHtml', () => {
             collapseWhitespace: true
         }))
         .pipe(rename('index.html'))
-        .pipe(gulp.dest(''));
+        .pipe(gulp.dest(''))
+        .pipe(livereload());
 });
 
 gulp.task('concatCss', () => {
@@ -55,19 +58,13 @@ gulp.task('cleanCss', () => {
     return gulp.src('src/home.css')
         .pipe(cleanCSS())
         .pipe(rename('index.css'))
-        .pipe(gulp.dest(''));
+        .pipe(gulp.dest(''))
+        .pipe(livereload());
 });
 
 gulp.task('concatJs', () => {
     return gulp.src('src/scripts/*.js')
         .pipe(concat('index.js'))
-        .pipe(gulp.dest(''));
-});
-
-gulp.task('cleanJs', () => {
-    return gulp.src('src/home.js')
-        .pipe(uglify())
-        .pipe(rename('index.js'))
         .pipe(gulp.dest(''));
 });
 
@@ -80,10 +77,13 @@ gulp.task('clean', () => {
 });
 
 gulp.task('watch', () => {
+    livereload.listen();
     gulp.watch('./src/styles/*.css', ['default']);
     gulp.watch('./src/pages/ru/*.htm', ['default']);
-    gulp.watch('./src/mainPage.htm', ['default']);
+    gulp.watch('./src/pages/mainPage.htm', ['default']);
 });
 
-gulp.task('default', gulpSequence(['concatHtml', 'concatCss', 'concatJs'], ['clearHtml', 'cleanCss', 'cleanJs'], 'clean'));
-gulp.task('dev', gulpSequence('default', 'watch'));
+gulp.task('dev', callback => {
+    gulpSequence(['concatHtml', 'concatCss', 'concatJs'], ['clearHtml', 'cleanCss', 'cleanJs'],'clean')(callback)
+});
+gulp.task('default', gulpSequence('default', 'watch'));
